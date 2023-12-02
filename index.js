80d729cb08615beeee5287a1067aaa4e96e1beb0
KarienyeRobert <karienyerobert@gmail.com>
2023-12-02 16:53:12 +0300

    commit

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  //menu icon navbar
+let menuIcon= document.querySelector('#menu-icon')
+let navbar= document.querySelector('.navbar')
+
+menuIcon.onclick=()=>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+//scroll sections active link
+let sections =document.querySelectorAll('section');
+let navLinks =document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+  sections.forEach(sec=>{
+    let top =window.scrollY;
+    let offset=sec.offsetTop - 150;
+    let height=sec.offsetHeight;
+    let id=sec.getAttribute('id');
+
+    if( top>=offset && top< offset+height) {
+         navLinks.forEach(links=>{
+           links.classList.remove('active');
+           document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+         });
+    };
+  });
+}
+
+  //dark light mode
+let darkModeIcon =document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick=()=>{
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode')
+}
